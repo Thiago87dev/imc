d81c571f7ba4b737572b1fa0c1678cc4934fb0ff
thiago alves <thiago87gsa@gmail.com>
2023-06-07 11:54:52 -0300

    corrigindo texto imc

--- main.js
+++ main.js
@@ -24,27 +24,27 @@ function programaImc (){
             if(imc < 18.5){
                 res.style.display = 'flex'
                 res.style.backgroundColor = 'green'
-                resTexto.innerHTML = `Seu icm é ${imc} (Abaixo do peso)`
+                resTexto.innerHTML = `Seu imc é ${imc} (Abaixo do peso)`
             }else if(imc >= 18.5 && imc <= 24.9){
                 res.style.display = 'flex'
                 res.style.backgroundColor = 'green'
-                resTexto.innerHTML = `Seu icm é ${imc} (Peso normal)`
+                resTexto.innerHTML = `Seu imc é ${imc} (Peso normal)`
             }else if(imc >= 25 && imc <= 29.9){
                 res.style.display = 'flex'
                 res.style.backgroundColor = 'green'
-                resTexto.innerHTML = `Seu icm é ${imc} (Sobrepeso)`
+                resTexto.innerHTML = `Seu imc é ${imc} (Sobrepeso)`
             }else if(imc >= 30 && imc <= 34.9){
                 res.style.display = 'flex'
                 res.style.backgroundColor = 'green'
-                resTexto.innerHTML = `Seu icm é ${imc} (Obesidade grau 1)`
+                resTexto.innerHTML = `Seu imc é ${imc} (Obesidade grau 1)`
             }else if(imc >= 35 && imc <= 39.9){
                 res.style.display = 'flex'
                 res.style.backgroundColor = 'green'
-                resTexto.innerHTML = `Seu icm é ${imc} (Obesidade grau 2)`
+                resTexto.innerHTML = `Seu imc é ${imc} (Obesidade grau 2)`
             }else{
                 res.style.display = 'flex'
                 res.style.backgroundColor = 'green'
-                resTexto.innerHTML = `Seu icm é ${imc} (Obesidade grau 3)`
+                resTexto.innerHTML = `Seu imc é ${imc} (Obesidade grau 3)`
             }   
         }     
     })
